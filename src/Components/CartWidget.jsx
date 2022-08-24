b4810016964724f4cde5cdc6e8716498a6874b96
Facundo N Godoy <facu14_23@hotmail.com>
2022-08-23 23:25:50 -0300

    NavBar y carrito

---
+++ src/Components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
+
+export default function CartWidget() {
+  return (
+    <>
+      <LocalMallIcon style={{ color: "#000", width: "3rem", height: "3rem" }} />
+    </>
+  );
+}
