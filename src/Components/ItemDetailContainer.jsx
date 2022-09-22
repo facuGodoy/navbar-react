773895e15659271179a2670125ea26bcfd52057a
Facundo N Godoy <facu14_23@hotmail.com>
2022-09-21 22:49:07 -0300

    "Integrando ItemCollection y FireBase"

--- src/Components/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { collection, getDocs, query, where, doc } from "firebase/firestore";
+import { collection, getDoc, doc } from "firebase/firestore";
 import { useParams } from "react-router-dom";
-import { data } from "./helper/Producto.js";
+// import { data } from "./helper/Producto.js";
 import ItemDetail from "./ItemDetail";
 import Loader from "./helper/Loader.js";
 import { db } from "./helper/FireBase.js";
@@ -15,14 +15,17 @@ export default function ItemDetailContainer() {
   useEffect(() => {
     setLoading(true);
     const CollectionProd = collection(db, "Products");
-    const refDoc = doc(CollectionProd, id);
-    getDocs(refDoc).then((result) => {
-      setProductDetail({ id: result.id, ...result.data() });
-    }).catch((err) => {
-      console.log(err);
-    }).finally(() => {
-      setLoading(false);
-    });
+    const referenceDoc = doc(CollectionProd, id);
+    getDoc(referenceDoc)
+      .then((result) => {
+        setProductDetail({ id: result.id, ...result.data() });
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, [id]);
 
   // useEffect(() => {
@@ -32,7 +35,5 @@ export default function ItemDetailContainer() {
   //     .finally(() => setLoading(false));
   // }, [id]);
 
-  return <div style={{ textAlign: "center" }}>
-    {loading ? <Loader /> : <ItemDetail productDetail={productDetail} />}
-  </div>;
+  return <div style={{ textAlign: "center" }}>{loading ? <Loader /> : <ItemDetail productDetail={productDetail} />}</div>;
 }
